0af62f493fe7caff3f95aed788bc9166dd9214c6
NathaliaPino <101751308+NathaliaPino@users.noreply.github.com>
2023-07-12 18:36:18 -0300

    #

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, FormEvent } from 'react'
+import { useContext, FormEvent, useState } from 'react'
 
 import Head from 'next/head'
 import Image from 'next/image';
@@ -8,6 +8,7 @@ import logoImg from '../../public/logo.svg';
 
 import { Input } from '../components/ui/Input'
 import { Button } from '../components/ui/Button'
+import { toast } from 'react-toastify'
 
 import { AuthContext } from '../contexts/AuthContext'
 
@@ -16,15 +17,29 @@ import Link from 'next/link';
 export default function Home() {
   const { signIn } = useContext(AuthContext)
 
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('');
+
+  const [loading, setLoading] = useState(false);
+
   async function handleLogin(event: FormEvent){
     event.preventDefault();
 
+    if(email === '' || password === ''){
+      toast.error("Preencha os campos")
+      return;
+    }
+
+    setLoading(true);
+
     let data = {
-      email: "algum@teste.com",
-      password: "123123"
+      email,
+      password
     }
 
     await signIn(data)
+
+    setLoading(false);
   }
 
   return (
@@ -40,22 +55,26 @@ export default function Home() {
           <Input
             placeholder="Digite seu email"
             type="text"
+            value={email}
+            onChange={ (e) => setEmail(e.target.value) }
           />
 
           <Input
             placeholder="Sua senha"
             type="password"
+            value={password}
+            onChange={ (e) => setPassword(e.target.value) }
           />
           
           <Button
             type="submit"
-            loading={false}
+            loading={loading}
           >
             Acessar
           </Button>
         </form>
 
-        <Link href="/signup" legacyBehavior>
+        <Link legacyBehavior href="/signup">
            <a className={styles.text}>Nao possui uma conta? Cadastre-se</a>
         </Link>
 
